8f84ef3c0627dacea9870c97e18e5a972fbdecfb
Eskaine <eskaineliu@gmail.com>
2022-11-24 22:50:23 +0800

    initial setup

---
+++ relayer_api/src/index.ts
@@ -0,0 +1,12 @@
+import express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () =>
+    // eslint-disable-next-line no-console
+    console.log(`Express is listening at http://localhost:${port}`)
+);
