2db6a897101d273261daedda46943348d86e1a2d
Eskaine <eskaineliu@gmail.com>
2022-11-26 22:28:07 +0800

    chore: update api index

--- relayer_api/src/index.ts
+++ relayer_api/src/index.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express';
 import relayerRouter from './routes/relayer.route';
+import errorHandler from './middlewares/errorHandler';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => res.send('API is working!'));
 app.use('/api', relayerRouter);
+app.use(errorHandler);
 
 app.listen(port, () =>
     // eslint-disable-next-line no-console
